27c32186532848d234c3dea6f89882b2edac7622
Akshat Tiwari <akshatt794@gmail.com>
2025-07-16 00:57:35 +0530

    feat: Add COD Available | Express Shipping topbar above header

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -3,9 +3,29 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { ScrollToTop } from "../components";
 
+// Topbar component right here for simplicity
+const Topbar = () => (
+  <div
+    style={{
+      width: "100%",
+      background: "#294143",
+      color: "#fff",
+      textAlign: "center",
+      padding: "7px 0",
+      fontSize: "16px",
+      letterSpacing: "1px",
+      fontWeight: 500,
+      zIndex: 999,
+    }}
+  >
+    COD Available | Express Shipping (PAN INDIA)
+  </div>
+);
+
 const HomeLayout = () => {
   return (
     <>
+      <Topbar />
       <ScrollToTop />
       <Header />
       <Outlet />
@@ -13,4 +33,5 @@ const HomeLayout = () => {
     </>
   );
 };
+
 export default HomeLayout;
